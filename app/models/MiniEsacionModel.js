a083d6c958b6234445bb4adb50e4e5b3f80b78df
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-03-02 20:47:52 +0100

    add new model

---
+++ app/models/MiniEsacionModel.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, Sequelize) => {
+  const miniestacion = sequelize.define('medidas', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    dispositivo: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    hora: {
+      type: Sequelize.DATE,
+      allowNull: true
+    },
+    temperatura: {
+      type: Sequelize.FLOAT,
+      allowNull: true
+    },
+    altura:{
+      type: Sequelize.FLOAT,
+      allowNull: true
+    },
+    presion:{
+      type: Sequelize.FLOAT,
+      allowNull: true
+    },
+    luxes: {
+      type: Sequelize.FLOAT,
+      allowNull: true
+    },
+  },
+    {
+      timestamps: true,
+    });
+
+  return miniestacion;
+}
