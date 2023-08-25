ca246304bbab9eb97142d7b4f7942abcdd027f0e
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-08-25 12:02:11 +0200

    updated

--- downloadfiles.sh
+++ downloadfiles.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# Nombre del contenedor
-CONTAINER_NAME="n8n"
-
-# Comando para ejecutar dentro del contenedor
-COMMAND_TO_RUN="/bin/sh"
-
-# Directorio de destino en el host para copiar los archivos
-DESTINATION_DIR="./csv"
-
-# Ejecutar el comando en el contenedor
-docker exec -it "$CONTAINER_NAME" "$COMMAND_TO_RUN"
-
-# Crear el directorio de destino en el host si no existe
-mkdir -p "$DESTINATION_DIR"
+echo "Eecjutando scritp"
 
 # Copiar archivos del contenedor al host
-docker cp "$CONTAINER_NAME:/home/node/temperatura" "$DESTINATION_DIR"
+docker cp n8n:/home/node/temperatura.csv ./ 
+
+echo "Archivos copiados"
