85c713bb22f833632e5a9683b469065d9b72158e
Enrique Rodriguez Vela <enriquemondejar15@gmail.com>
2024-03-06 13:14:32 +0100

    Update database configuration

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,8 @@
 const database = "miniestacion";
 const user = "postgres";
-<<<<<<< HEAD
 const password = "postgre";
 const host = "localhost"; // This should be the name of your PostgreSQL Docker container
 const port = "5432";
-=======
-const password = "tycgis";
-const host = "192.168.1.127";
-// const host = "db"; // This should be the name of your PostgreSQL Docker container
-const port = "15432";
->>>>>>> 0b331ee71fc4d0f28bb6cadd381b9fe65c10555b
 
 module.exports = {
     HOST: host,
